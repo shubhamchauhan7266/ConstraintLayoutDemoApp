8ea70268e3c0da4355a46421e40f33981b9171bf
Shubham Chauhan <shubham.chauhan@kelltontech.com>
2018-07-26 12:04:44 +0530

    Initial commit

---
+++ app/src/main/java/com/ownwork/constraintlayoutdemoapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ownwork.constraintlayoutdemoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
