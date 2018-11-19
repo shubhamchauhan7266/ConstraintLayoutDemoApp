84363779f82272f229ef2583c670376e022addd8
Shubham Chauhan <shubham.chauhan@kelltontech.com>
2018-11-19 14:24:26 +0530

    + initial commit

--- app/src/main/java/com/ownwork/constraintlayoutdemoapp/MainActivity.kt
+++ app/src/main/java/com/ownwork/constraintlayoutdemoapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.ownwork.constraintlayoutdemoapp
 
 import android.support.v7.app.AppCompatActivity
+import android.os.Handler
+import jp.wasabeef.blurry.Blurry
+import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Handler().postDelayed({
+            Blurry.with(this)
+                    .radius(30)
+                    .sampling(2)
+//                    .color(Color.argb(66, 255, 255, 0))
+                    .animate()
+                    .onto(root_view)
+        }, 5 * 1000)
     }
 }
